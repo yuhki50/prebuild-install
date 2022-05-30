d1853cbd75167140945b85c290ace8194c9db87d
Lovell Fuller <lovell@users.noreply.github.com>
2022-05-30 19:47:07 +0100

    Ensure script output can be captured by tests (#181)
    Replaces temporary logfile workaround

--- test/skip-test.js
+++ test/skip-test.js
@@ -33,7 +33,6 @@ test('does not skip download in standalone package', function (t) {
 
 function run (t, mode, args, cb) {
   const addon = tempy.directory()
-  const logfile = path.join(addon, 'prebuild-install.log')
   let cwd = addon
 
   writePackage(addon, {
@@ -62,19 +61,13 @@ function run (t, mode, args, cb) {
     npm_config_addon_binary_host: 'http://localhost:1234',
     npm_config_prefer_offline: 'true',
     npm_config_audit: 'false',
-
-    // Temporary workaround for npm 7 which swallows our output
-    npm_config_prebuild_install_logfile: logfile,
+    npm_config_foreground_scripts: true,
     npm_config_loglevel: 'info'
   })
 
-  exec(npm + ' install', { cwd, env }, function (err) {
+  exec(npm + ' install', { cwd, env }, function (err, stdout, stderr) {
     t.ifError(err, 'no install error')
-
-    fs.readFile(logfile, 'utf8', function (err, data) {
-      t.ifError(err, 'no read error')
-      cb(logs(data))
-    })
+    cb(logs(stderr))
   })
 }
 
